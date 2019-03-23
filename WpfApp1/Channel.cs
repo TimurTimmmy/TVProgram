f35a79109d2df778a064a0ea40d6269f7d9b3424
Timur <ash123@mail.ru>
2019-03-23 15:28:13 +0300

    private Channel

--- WpfApp1/Channel.cs
+++ WpfApp1/Channel.cs
@@ -1,13 +1,36 @@
 ﻿namespace TVProgram
 {
-    class Channel
+    public class Channel
     {
+        private string id;
+        private string name;
+        private string logo;
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Logo
+        {
+            get { return logo; }
+            set { logo = value; }
+        }
         public Channel(string Id, string Name)
         {
             this.Id = Id;
             this.Name = Name;
+            this.Logo = "http://www.americanlayout.com/wp/wp-content/uploads/2012/08/C-To-Go-300x300.png";
         }
-        public string Id { get; set; }
-        public string Name { get; set; }
+        public Channel(string Id, string Name, string Logo)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Logo = Logo;
+        }
     }
 }
